5ed4cb78d033e596829950d3365ae5f4c892af91
Urszula Molska <urszula_molska@wp.pl>
2022-10-02 14:22:16 +0200

    task nr 1 update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    galleryItem =>
+      `<a class="gallery__item" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"/></a>`
+  )
+  .join('');
+const toHtml = gallery.insertAdjacentHTML('afterbegin', markup);
 
 console.log(galleryItems);
+
+let LightboxGallery = new SimpleLightbox('.gallery a');
+LightboxGallery.on('show.simplelightbox');
+//captionsData;
+//captionPosition;
+//captionDelay;
+
+LightboxGallery.defaultOptions.captionsData = 'alt';
+
+LightboxGallery.defaultOptions.captionDelay = 250;
+document.addEventListener('keyup', event => {
+  if (event.code === 'Escape') {
+    LightboxGallery.close;
+  }
+});
