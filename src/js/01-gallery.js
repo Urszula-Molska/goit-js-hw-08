73f29501c00916722d2e57f12cd1b5d33bc42050
Urszula Molska <urszula_molska@wp.pl>
2022-10-02 16:09:15 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from '../../node_modules/simplelightbox';
-const SimpleLightbox = require('simplelightbox');
+import SimpleLightbox from 'simplelightbox';
+//import SimpleLightbox from '../../node_modules/simplelightbox';
+//const SimpleLightbox = require('simplelightbox');
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
