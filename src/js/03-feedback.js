b6654a9d17a2005489a6555ae322d2a6eaf541a0
Urszula Molska <urszula_molska@wp.pl>
2022-10-06 22:46:16 +0200

    task nr 3 updated

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,7 @@
+import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
-
-form.addEventListener('submit', formCheck);
-
-function formCheck(event) {
-  event.preventDefault();
-  if (input.value === '' || textarea.value === '') {
-    return alert('Please fill in all the fields!');
-  }
-}
 
 let email = '';
 let message = '';
@@ -26,3 +18,30 @@ textarea.addEventListener('input', event => {
   userCredentials.message = event.currentTarget.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(userCredentials));
 });
+
+const savedUserCredentials = localStorage.getItem('feedback-form-state');
+const parsedUserCredentials = JSON.parse(savedUserCredentials);
+const updateCredentials = () => {
+  if (localStorage.getItem('feedback-form-state') === null) {
+    return;
+  } else {
+    form.elements.email.value = parsedUserCredentials.email;
+    form.elements.message.value = parsedUserCredentials.message;
+  }
+};
+
+updateCredentials();
+
+form.addEventListener('submit', formCheck);
+
+function formCheck(event) {
+  event.preventDefault();
+  if (input.value === '' || textarea.value === '') {
+    return alert('Please fill in all the fields!');
+  } else {
+    console.log(parsedUserCredentials);
+    localStorage.clear();
+    form.elements.email.value = '';
+    form.elements.message.value = '';
+  }
+}
